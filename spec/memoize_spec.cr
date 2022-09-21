47496f814b9fd795015d3f25cbf568b9936af582
tkshnwesper <tkshnwesper@users.noreply.github.com>
2022-09-21 16:38:09 +0530

    add memoizing for only set of parameters

--- spec/memoize_spec.cr
+++ spec/memoize_spec.cr
@@ -1,20 +1,44 @@
 require "./spec_helper"
 
-CALLS = [] of Int32
+class TestMemoizeMethod
+  getter calls
 
-class TestMemoize
+  def initialize
+    @calls = [] of Int32
+  end
+
   Memoize.memoize add_two, NamedTuple(n: Int32), Int32 do
-    CALLS << n
+    @calls << n
     n + 2
+  end
+end
+
+class TestMemoizeOnlyMethod
+  getter calls
+
+  def initialize
+    @calls = 0
+  end
+
+  Memoize.memoize_only search, NamedTuple(db: Object), NamedTuple(query: String), String do
+    @calls += 1
+    query + query
   end
 end
 
 describe Memoize do
   it "runs memoized method only once for same parameters" do
-    tm = TestMemoize.new
+    tm = TestMemoizeMethod.new
     tm.add_two(6).should eq 8
     tm.add_two(6).should eq 8
     tm.add_two(6).should eq 8
-    CALLS.should eq [6]
+    tm.calls.should eq [6]
+  end
+
+  it "memoizes method only for certain parameters" do
+    tmo = TestMemoizeOnlyMethod.new
+    tmo.search(1, "hi")
+    tmo.search(2, "hi")
+    tmo.calls.should eq 1
   end
 end
--- src/memoize.cr
+++ src/memoize.cr
@@ -28,7 +28,7 @@
 # end
 # ```
 module Memoize
-  VERSION = "1.0.0"
+  VERSION = "1.2.0"
 
   # Macro which creates a memoized function without any space bounds (infinitely large cache).
   #
@@ -42,6 +42,30 @@ module Memoize
   # add_two(5) # returns 7
   # add_two(5) # returns 7
   # ```
+  macro memoize_method(method_name, param_tuple, return_type, &block)
+    {% param_named_tuple = param_tuple.named_args %}
+    {% param_list = param_named_tuple.keys %}
+    {% type_list = param_named_tuple.values %}
+    {% def_param_list = param_named_tuple.map { |param, type| "#{param} : #{type}" }.join(", ").id %}
+
+    @CACHE_{{ method_name }} = {} of {{ *type_list }} => {{ return_type }}
+
+    def _{{ method_name }}({{ def_param_list }}) : {{ return_type }}
+        {{ yield }}
+    end
+
+    def {{ method_name }}({{ def_param_list }}) : {{ return_type }}
+        if @CACHE_{{ method_name }}.has_key?({{ *param_list }})
+            @CACHE_{{ method_name }}[{{ *param_list }}]
+        else
+            @CACHE_{{ method_name }}[{{ *param_list }}] = _{{ method_name }}({{ *param_list }})
+        end
+    end
+  end
+
+  # Like `memoize_method` but can be used in any scope (class, module, global).
+  # If using in class, the memoization will happen at class level meaning, memoized objects can
+  # be accessed between different instances of the class.
   macro memoize(method_name, param_tuple, return_type, &block)
     {% param_named_tuple = param_tuple.named_args %}
     {% param_list = param_named_tuple.keys %}
@@ -59,6 +83,66 @@ module Memoize
             @CACHE_{{ method_name }}[{{ *param_list }}]
         else
             @CACHE_{{ method_name }}[{{ *param_list }}] = _{{ method_name }}({{ *param_list }})
+        end
+    end
+  end
+
+  # Only memoizes certain parameters of a method.
+  # Useful when certain parameters do not have equals and hash implemented,
+  # but memoization is still necessary on the other parameters.
+  # `rest_tuple` should be a `NamedTuple` containing parameters that should **not** be memoized.
+  # `only_tuple` should be a `NamedTuple` containing parameters that need to be memoized.
+  macro memoize_only_method(method_name, rest_tuple, only_tuple, return_type, &block)
+    {% rest_named_tuple = rest_tuple.named_args %}
+    {% rest_param_list = rest_named_tuple.keys %}
+    {% rest_def_param_list = rest_named_tuple.map { |param, type| "#{param} : #{type}" }.join(", ").id %}
+
+    {% only_named_tuple = only_tuple.named_args %}
+    {% only_param_list = only_named_tuple.keys %}
+    {% only_type_list = only_named_tuple.values %}
+    {% only_def_param_list = only_named_tuple.map { |param, type| "#{param} : #{type}" }.join(", ").id %}
+
+    @CACHE_{{ method_name }} = {} of {{ *only_type_list }} => {{ return_type }}
+
+    def _{{ method_name }}({{ rest_def_param_list }}, {{ only_def_param_list }}) : {{ return_type }}
+        {{ yield }}
+    end
+
+    def {{ method_name }}({{ rest_def_param_list }}, {{ only_def_param_list }}) : {{ return_type }}
+        if @CACHE_{{ method_name }}.has_key?({{ *only_param_list }})
+            @CACHE_{{ method_name }}[{{ *only_param_list }}]
+        else
+            @CACHE_{{ method_name }}[{{ *only_param_list }}] = _{{ method_name }}(
+              {{ *rest_param_list }}, {{ *only_param_list }})
+        end
+    end
+  end
+
+  # Like `memoize_only_method` but can be used in any scope (module, class, global).
+  # If using in class, the memoization will happen at class level meaning, memoized objects can
+  # be accessed between different instances of the class.
+  macro memoize_only(method_name, rest_tuple, only_tuple, return_type, &block)
+    {% rest_named_tuple = rest_tuple.named_args %}
+    {% rest_param_list = rest_named_tuple.keys %}
+    {% rest_def_param_list = rest_named_tuple.map { |param, type| "#{param} : #{type}" }.join(", ").id %}
+
+    {% only_named_tuple = only_tuple.named_args %}
+    {% only_param_list = only_named_tuple.keys %}
+    {% only_type_list = only_named_tuple.values %}
+    {% only_def_param_list = only_named_tuple.map { |param, type| "#{param} : #{type}" }.join(", ").id %}
+
+    CACHE_{{ method_name }} = {} of {{ *only_type_list }} => {{ return_type }}
+
+    def _{{ method_name }}({{ rest_def_param_list }}, {{ only_def_param_list }}) : {{ return_type }}
+        {{ yield }}
+    end
+
+    def {{ method_name }}({{ rest_def_param_list }}, {{ only_def_param_list }}) : {{ return_type }}
+        if CACHE_{{ method_name }}.has_key?({{ *only_param_list }})
+            CACHE_{{ method_name }}[{{ *only_param_list }}]
+        else
+            CACHE_{{ method_name }}[{{ *only_param_list }}] = _{{ method_name }}(
+              {{ *rest_param_list }}, {{ *only_param_list }})
         end
     end
   end
